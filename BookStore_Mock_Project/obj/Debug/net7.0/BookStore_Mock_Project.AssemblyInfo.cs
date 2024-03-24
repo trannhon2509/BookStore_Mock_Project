746844efb9d0df530068841811e2bd88a708011d
Nhan <nhanptde160251@fpt.edu.vn>
2024-03-24 22:24:33 +0900

    Done Category

--- BookStore_Mock_Project/obj/Debug/net7.0/BookStore_Mock_Project.AssemblyInfo.cs
+++ BookStore_Mock_Project/obj/Debug/net7.0/BookStore_Mock_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore_Mock_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda2eb7eaa82ed8acf82532eee51ed2c56c17f91")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore_Mock_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore_Mock_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
